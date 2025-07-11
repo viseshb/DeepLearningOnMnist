d2cf6d5d856f4de38b78c92d047e429296c055f6
viseshb <visesh66@gmail.com>
2025-07-10 21:59:12 -0500

    add new files

--- Algorithms/bubble_sort.py
+++ Algorithms/bubble_sort.py
@@ -1,18 +1,15 @@
 def bubble_sort(arr):
      n = len(arr)
-
      for i in range(n):
-          swapped = False
-          for j in range(0,n-i-1):
-               if arr[j]>arr[j+1]:
-                    arr[j], arr[j+1] = arr[j+1],arr[j]
-                    swapped =True
-
-          if not swapped:
-               break
+        swapped = False
+        for j in range(0, n-i-1):
+            if arr[j] > arr[j+1]:
+               arr[j], arr[j+1] = arr[j+1], arr[j]
+               swapped = True
+        if not swapped:
+            break
      return arr
 
-
-arr = [64, 34, 25, 12, 22, 11, 90]
+arr = [5,4,3,2,1]
 bubble_sort(arr)
-print(arr)
+print(arr)         
