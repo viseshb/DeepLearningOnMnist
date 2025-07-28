038911671de95b1fc6ee8dc58146d223f10eaef4
viseshb <visesh66@gmail.com>
2025-07-28 15:00:28 -0500

    add new files

---
+++ Python Course with Notes/1. Chapter 1/01_hello.py
@@ -0,0 +1,28 @@
+# '''
+# Author: Harry
+# Licenced to: ABC Company
+# ***********Thanks for reading**********
+# '''
+# import os # importing the os module 
+# print("Hello world")
+# Print a greeting
+import os
+from playsound import playsound
+playsound('C:\\Users\\vises\\OneDrive\\Desktop\\play.mp3')
+
+print("Hello, World!")
+
+# Variables and data types
+name = "Visesh"
+age = 25
+is_student = True
+
+# Conditional statement
+if is_student:
+ print(f"{name} is a student.")
+else:
+ print(f"{name} is not a student.")
+
+print("Hello World.")
+print("Twinkle Twinkle Little Star")
+
---
+++ Python Course with Notes/1. Chapter 1/03_pr_03_module_usage.py
@@ -0,0 +1,2 @@
+from playsound import playsound
+playsound('C:\\Users\\vises\\OneDrive\\Desktop\\Python Course With Notes\\1. Chapter 1\\play.mp3')
